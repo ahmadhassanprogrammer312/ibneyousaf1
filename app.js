7ca8f84d9a22e46c4dadd2e8117b342cd184d1bd
Ahmad Hassan <ahmadhassan.org@gmail.com>
2025-02-21 18:54:28 +0500

    new

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-    res.render('home');
-});
 app.get('/digital-library', (req, res) => {
     // res.setHeader('Content-Type', 'text/html');
     res.send('digitalLibrary');
 });
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
