9cced1d9b7df1569ce665856bfda2edc20efac0f
Ahmad Hassan <ahmadhassan.org@gmail.com>
2025-02-21 20:36:27 +0500

    setup views directory

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
+
 const port = 3000;
 
 app.set('view engine', 'ejs');
-
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -15,6 +18,6 @@ app.get('/digitallibrary', (req, res) => {
 });
 
 
-// app.listen(port, () => {
-//     console.log(`App listening at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
