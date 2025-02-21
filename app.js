d5a2214709bc8c56fbae80924be132ac6cf8e667
Ahmad Hassan <ahmadhassan.org@gmail.com>
2025-02-21 22:41:59 +0500

    updated

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ const port = 3000;
 // Set the views directory
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.get('/digitallibrary', (req, res) => {
     // res.setHeader('Content-Type', 'text/html');
-    res.send('digitalLibrary');
+    res.render('digitalLibrary');
 });
 app.get('/about', (req, res) => {
 
